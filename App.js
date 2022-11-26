079420fed4d09f1106d6b3fc4bf9b56b6ae0a5dc
unknown <juniorldjr@gmail.com>
2022-11-25 21:43:20 -0400

    Final

--- App.js
+++ App.js
@@ -1,16 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
+import {Component,useState} from "react";
 import * as Location from 'expo-location';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView , { Callout, Marker, Polyline,provider } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-const carImage = require('./assets/auto.png')
+
+const ubiImage = require('./assets/ubicacion.png')
+const cravyImage = require('./assets/tienda.png')
+ 
+
 export default function App() {
 
+  
+  const [MapData, duration] = React.useState({
+    distance:0,
+    duration: 0,
+  });
   const [origin, setOrigin] = React.useState({
     latitude:-17.748811692293902,
     longitude:  -63.1493895583319,
+  });
+
+  const [cravy] = React.useState({
+    latitude:-17.41358328135466,
+    longitude:  -66.16623603376084
+   
+  });
+
+  const [cravy1] = React.useState({
+    latitude:-17.767630424398185,
+    longitude:  -63.15818800553231
+    
+  });
+
+  const [cravy2] = React.useState({
+    latitude:   -17.76675060727519,
+    longitude:  -63.19123690819468
+  
+  });
+
+  const [cravy3] = React.useState({
+    latitude:-17.750677297645375,
+    longitude: -63.171870104179085
+    
+  });
+
+  const [cravy4] = React.useState({
+    latitude:-17.782584442337782,
+    longitude: -63.17189681902508
+    
   });
 
   const [destination, setDestination] = React.useState({
@@ -30,6 +70,7 @@ export default function App() {
     latitudeDelta: 0.09,
    longitudeDelta: 0.04
   });
+
 
   React.useEffect(() => {
     getLocationPermission();
@@ -51,6 +92,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+       
       <Text>
       <GooglePlacesAutocomplete 
       style={styles.pkaces}
@@ -82,6 +124,7 @@ export default function App() {
 </Text>
       
       <MapView
+      // customMapStyle={mapStyle}
       style={styles.map2} 
       initialRegion={{
         latitude: origin.latitude,
@@ -89,28 +132,102 @@ export default function App() {
         latitudeDelta: 0.09,
         longitudeDelta: 0.04
       }}
+      
+      mapType="standard"
       provider="google"
       >
 
         
       <Marker
-      draggable
+      //draggable
       coordinate={origin}
-      //image={carImage}
-      onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}>
+      
+      image={ubiImage}
+     //</MapView> onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+     >
         <Callout>
           <Text>Mi ubicacion</Text>
          </Callout>
         
       </Marker>
 
+      <Marker
+      //draggable
+      coordinate={cravy}
+      image={cravyImage}
+      onPress={(direction) => setRegion(direction.nativeEvent.coordinate)}
+      >
+        <Callout>
+          <Text>Sucursal Chb</Text>
+         </Callout>
+        
+      </Marker>
+
+      <Marker
+      //draggable
+      coordinate={cravy1}
+      image={cravyImage}
+      onPress={(direction) => setRegion(direction.nativeEvent.coordinate)}
+      >
+        <Callout>
+          <Text>Sucursal</Text>
+         </Callout>
+        
+      </Marker>
+
+      <Marker
+      //draggable
+      coordinate={cravy2}
+      image={cravyImage}
+      onPress={(direction) => setRegion(direction.nativeEvent.coordinate)}
+      >
+        <Callout>
+          <Text>Sucursal</Text>
+         </Callout>
+        
+      </Marker>
+
+      <Marker
+      //draggable
+      coordinate={cravy3}
+      image={cravyImage}
+      onPress={(direction) => setRegion(direction.nativeEvent.coordinate)}
+      >
+        <Callout>
+          <Text>Sucursal</Text>
+         </Callout>
+        
+      </Marker>
+
+      <Marker
+      //draggable
+      onPress={(direction) => setRegion(direction.nativeEvent.coordinate)}
+      coordinate={cravy4}
+      image={cravyImage}
+      //</MapView>onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+      >
+        <Callout>
+          <Text>1</Text>
+         </Callout>
+        
+      </Marker>
+
+
+
+
+
       <Marker 
-      
+         // onPress={(direction) => setOrigin(direction.nativeEvent.coordinate)}
           draggable
           coordinate={{latitude: region.latitude, longitude: region.longitude}}
           onDragEnd= {(direction) => setRegion(direction.nativeEvent.coordinate)}
           //onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
-      />
+      >
+        <Callout>
+          <Text>Destino</Text>
+         </Callout>
+      </Marker>
+
 
 <MapViewDirections
          origin={origin}
@@ -118,12 +235,25 @@ export default function App() {
          apikey={'AIzaSyCyh5gh3bBe9VhQqxFaRrEtVbuWOIZcqo8'}
          strokeColor="black"
          strokeWidth={5}
+         onReady={result =>{
+          MapData.distance = result.distance
+          MapData.duration = result.duration
+        console.log(`Distance: ${MapData.distance} km`)
+         console.log(`Duration: ${MapData.duration} min.`)
+         }}
          />
-
-
-
-
+         
       </MapView>
+      <View style={styles.container2}>
+      <Text>
+                 km: {Math.round(MapData.distance)}
+                 
+          </Text>
+          <Text>
+                 Minutos: {Math.round(MapData.duration)}
+               
+          </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -134,8 +264,18 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop : 50,
   },
-  map2:{
-    width: '100%',
-    height: '100%'
+  container2:{
+    flex: 2,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //marginTop : 50,
   },
+  map2:{
+    flex: 16,
+    width: '100%',
+    height: '100%',
+    
+  },
+  
 });
